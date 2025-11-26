772b0f1cc3c6a9469e03f2ed13e4f4d74eb7236e
Dominic Nguyen <dominic.j.nguyen@gmail.com>
2025-11-25 23:18:10 -0800

    Removed extra outtake and intake commands, replaced with startEnd commands

--- src/main/java/frc/robot/commands/SuperstructureCommands.java
+++ src/main/java/frc/robot/commands/SuperstructureCommands.java
@@ -3,84 +3,36 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.superstructure.Superstructure;
-import frc.robot.subsystems.superstructure.Superstructurestate;
 
 public class SuperstructureCommands {
-    // Command to intake gamepiece with no time limits
+
+    // Command to return to idle state
+    public static Command returnToIdle(Superstructure superstructure) {
+        return Commands.runOnce(() -> superstructure.requestIdle(), superstructure)
+                .withName("ReturnToIdle");
+    }
+
     public static Command intake(Superstructure superstructure) {
-        return Commands.sequence(
-                        Commands.runOnce(() -> superstructure.requestIntake(), superstructure),
-                        Commands.waitUntil(() -> superstructure.hasGamePiece()),
-                        Commands.runOnce(() -> superstructure.requestIdle(), superstructure))
-                .withName("Intake")
-                .alongWith(Commands.run(() -> {}, superstructure.getIntake()));
-    }
-
-    // Command to intake gamepiece with timeout, stops trying to intake after timeout seconds no matter if it has
-    // gamepiece or not
-    public static Command intakeWithTimeout(Superstructure superstructure, double timeout) {
-        return Commands.sequence(
-                        Commands.runOnce(() -> superstructure.requestIntake(), superstructure),
-                        Commands.waitSeconds(timeout),
-                        Commands.runOnce(() -> superstructure.requestIdle(), superstructure))
-                .withName("IntakeWithTimeout")
-                .alongWith(Commands.run(() -> {}, superstructure.getIntake()));
-    }
-
-    // Command to manually run intake
-    public static Command manualIntake(Superstructure superstructure) {
         return Commands.startEnd(
-                        () -> superstructure.requestIntake(), () -> superstructure.requestIdle(), superstructure)
-                .withName("ManualIntake")
+                        () -> superstructure.requestIntake(), 
+                        () -> superstructure.requestIdle(), 
+                        superstructure)
+                .withName("intake")
                 .alongWith(Commands.run(() -> {}, superstructure.getIntake()));
-    }
-
-    // Command to outtake gamepiece
-    public static Command outtake(Superstructure superstructure, double duration) {
-        return Commands.sequence(
-                        Commands.runOnce(() -> superstructure.requestOuttake(), superstructure),
-                        Commands.waitSeconds(duration),
-                        Commands.runOnce(() -> superstructure.requestIdle(), superstructure))
-                .withName("Eject");
     }
 
     // Command to manually outtake
-    public static Command manualOuttake(Superstructure superstructure) {
+    public static Command outtake(Superstructure superstructure) {
         return Commands.startEnd(
-                        () -> superstructure.requestOuttake(), () -> superstructure.requestIdle(), superstructure)
-                .withName("ManualEject");
-    }
-
-    // Command to stow intake
-    public static Command stow(Superstructure superstructure) {
-        return Commands.runOnce(() -> superstructure.requestIdle(), superstructure)
-                .withName("Stow");
+                        () -> superstructure.requestOuttake(), 
+                        () -> superstructure.requestIdle(), 
+                        superstructure)
+                .withName("outtake");
     }
 
     // Command to wait until superstructure is ready
     public static Command waitForReady(Superstructure superstructure) {
         return Commands.waitUntil(() -> superstructure.isReady()).withName("WaitForReady");
-    }
-
-    // Command to prep for shooting
-    // REMMEBER TO FIX IMPLEMENTATION WHEN SHOOTER IS FULLY DONE
-    public static Command prepareToShoot(Superstructure superstructure) {
-        return Commands.sequence(
-                        Commands.runOnce(() -> superstructure.requestShootPrepare(), superstructure),
-                        Commands.waitUntil(
-                                () -> superstructure.getCurrentState() == Superstructurestate.PREPARING_TO_SHOOT
-                                        && superstructure.isReady()))
-                .withName("PrepareToShoot");
-    }
-
-    // Command to shoot
-    // REmember TO FIX IMPLEMENTATION WHEN SHOOTER IS FULLY DONE
-    public static Command shoot(Superstructure superstructure) {
-        return Commands.sequence(
-                        Commands.runOnce(() -> superstructure.requestShoot(), superstructure),
-                        Commands.waitSeconds(0.5),
-                        Commands.runOnce(() -> superstructure.requestIdle(), superstructure))
-                .withName("Shoot");
     }
 
     // Command to emergency stop the superstructure
