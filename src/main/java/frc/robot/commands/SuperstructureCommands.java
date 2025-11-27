716e180a69094ff82b4efb523e897de06e2b280b
Dominic Nguyen <dominic.j.nguyen@gmail.com>
2025-11-26 16:28:12 -0800

    Rephrased commmands to work with new superstructure code

--- src/main/java/frc/robot/commands/SuperstructureCommands.java
+++ src/main/java/frc/robot/commands/SuperstructureCommands.java
@@ -3,41 +3,33 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.superstructure.Superstructure;
+import frc.robot.subsystems.superstructure.Superstructure.Goal;
 
 public class SuperstructureCommands {
 
-    // Command to return to idle state
-    public static Command returnToIdle(Superstructure superstructure) {
-        return Commands.runOnce(() -> superstructure.requestIdle(), superstructure)
-                .withName("ReturnToIdle");
-    }
-
+    // Standard goal command - returns to STOW when released
     public static Command intake(Superstructure superstructure) {
         return Commands.startEnd(
-                        () -> superstructure.requestIntake(), 
-                        () -> superstructure.requestIdle(), 
-                        superstructure)
-                .withName("intake")
-                .alongWith(Commands.run(() -> {}, superstructure.getIntake()));
+                () -> superstructure.setGoal(Goal.INTAKING),
+                () -> superstructure.setGoal(Goal.IDLE),
+                superstructure)
+            .withName("Intake");
     }
 
-    // Command to manually outtake
     public static Command outtake(Superstructure superstructure) {
         return Commands.startEnd(
-                        () -> superstructure.requestOuttake(), 
-                        () -> superstructure.requestIdle(), 
-                        superstructure)
-                .withName("outtake");
+                () -> superstructure.setGoal(Goal.OUTTAKING),
+                () -> superstructure.setGoal(Goal.IDLE),
+                superstructure)
+            .withName("Outtake");
     }
 
-    // Command to wait until superstructure is ready
-    public static Command waitForReady(Superstructure superstructure) {
-        return Commands.waitUntil(() -> superstructure.isReady()).withName("WaitForReady");
-    }
-
-    // Command to emergency stop the superstructure
-    public static Command emergencyStop(Superstructure superstructure) {
-        return Commands.runOnce(() -> superstructure.emergencyStop(), superstructure)
-                .withName("EmergencyStop");
+    // Wait until goal is reached (for autonomous sequencing)
+    public static Command intakeUntilReady(Superstructure superstructure) {
+        return Commands.sequence(
+            Commands.runOnce(() -> superstructure.setGoal(Goal.INTAKING)),
+            Commands.waitUntil(Superstructure::atGoal)
+        ).finallyDo(() -> superstructure.setGoal(Goal.IDLE))
+         .withName("IntakeUntilReady");
     }
 }
