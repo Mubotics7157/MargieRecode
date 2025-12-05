6a805cca420166f9915690ebf3ccf584dc0c26de
Pramit Mazumder <pmazumder3927@gmail.com>
2025-12-05 03:43:00 -0800

    update mk5n constants

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -76,10 +76,10 @@ public class TunerConstants {
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
-    private static final double kCoupleRatio = 3.125;
+    private static final double kCoupleRatio = 3.375;
 
-    private static final double kDriveGearRatio = 675 / 128;
-    private static final double kSteerGearRatio = 18.75;
+    private static final double kDriveGearRatio = 675.0 / 128.0;
+    private static final double kSteerGearRatio = 26.09;
     private static final Distance kWheelRadius = Inches.of(1.841);
 
     private static final boolean kInvertLeftSide = false;
