fe2758c7b5fe2137836951b206a9ba9df9d9d867
527dj <157839604+527dj@users.noreply.github.com>
2025-12-08 18:13:24 -0800

    Added shooter

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,5 +1,63 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
-public class Shooter extends SubsystemBase {}
+public class Shooter extends SubsystemBase {
+    private final ShooterIO io;
+    private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+
+    private double targetVelocityRPM = 0.0;
+    private boolean shooterEnabled = false;
+
+    public Shooter(ShooterIO io) {
+        this.io = io;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Shooter", inputs);
+
+        // Control shooter based on enable state
+        if (shooterEnabled && targetVelocityRPM > 0) {
+            io.setVelocity(targetVelocityRPM, 12.0); // 12V feedforward
+        } else {
+            io.stop();
+        }
+
+        // Log shooter state
+        Logger.recordOutput("Shooter/TargetVelocityRPM", targetVelocityRPM);
+        Logger.recordOutput("Shooter/Enabled", shooterEnabled);
+        Logger.recordOutput("Shooter/AtSetpoint", atSetpoint());
+    }
+
+    public void setTargetVelocity(double velocityRPM) {
+        this.targetVelocityRPM = velocityRPM;
+    }
+
+    public void enable() {
+        shooterEnabled = true;
+    }
+
+    public void disable() {
+        shooterEnabled = false;
+        targetVelocityRPM = 0.0;
+    }
+
+    public boolean atSetpoint() {
+        if (!shooterEnabled || targetVelocityRPM == 0) {
+            return false;
+        }
+        double error = Math.abs(inputs.velocityRPM - targetVelocityRPM);
+        return error < 100; // Within 100 RPM
+    }
+
+    public double getVelocityRPM() {
+        return inputs.velocityRPM;
+    }
+
+    public boolean isEnabled() {
+        return shooterEnabled;
+    }
+}
--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,1 +1,23 @@
+package frc.robot.subsystems.shooter;
 
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ShooterIO {
+    @AutoLog
+    public static class ShooterIOInputs {
+        public double velocityRPM = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+        public double temperatureCelsius = 0.0;
+    }
+
+    public default void updateInputs(ShooterIOInputs inputs) {}
+
+    public default void setVoltage(double volts) {}
+
+    public default void setVelocity(double velocityRPM, double ffVolts) {}
+
+    public default void stop() {}
+
+    public default void setBrakeMode(boolean enable) {}
+}
